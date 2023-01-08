2d558f1276656a58947ed42475592672f3c9a0fc
Bartek <baqbaek@gmail.com>
2023-01-08 17:28:40 +0100

    first steps in objects

--- first_steps.py
+++ first_steps.py
@@ -1,11 +1,18 @@
 class PlayerCharacter():
+    membership = True  # Class Object Atribute
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
+    def shout(self):
+        print(f'My name is {self.name}')
 
 
 player1 = PlayerCharacter('Bartek', 22)
 player2 = PlayerCharacter('Lukas', 23)
 
-print(f'{player1.name} {player1.age}')
-print(player2.name)
+# print(f'{player1.name} {player1.age}')
+# print(player2.name)
+# print(player1.membership)
+print(player1.shout())
