030ce9c2e390a8095cbcec19e6a24eeff314a81b
Bartek <baqbaek@gmail.com>
2023-01-08 16:36:25 +0100

    first steps in objects

--- first_steps.py
+++ first_steps.py
@@ -2,8 +2,7 @@ class PlayerCharacter():
     def __init__(self, name, age):
         self.name = name
         self.age = age
-    def run(self):
-        print('run')
+
 
 player1 = PlayerCharacter('Bartek', 22)
 player2 = PlayerCharacter('Lukas', 23)
